9c3015e9b568f2a1caf6f57a613edfeff5854ab8
348337643 <348337643@C-HMCNJH2.tdsb.on.ca>
2026-05-14 11:13:33 -0400

    privacyCase completed + outline of comments

--- src/Verdict.java
+++ src/Verdict.java
@@ -12,42 +12,79 @@ public class Verdict {
     private String studentVerdict;
     private String reason;
     
+    /**
+     * 
+     */
     public Verdict() {
         caseTitle = "";
         studentVerdict = "";
         reason = "";
     }
     
+    /**
+     * 
+     * @param caseTitle
+     * @param verdict
+     * @param reason 
+     */
     public Verdict(String caseTitle, String verdict, String reason) {
         this.caseTitle = caseTitle;
         studentVerdict = verdict;
         this.reason = reason;
     }
     
+    /**
+     * 
+     * @param title 
+     */
     public void setCaseTitle(String title) {
         caseTitle = title;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public String getCaseTitle() {
         return caseTitle;
     }
     
+    /**
+     * 
+     * @param reason 
+     */
     public void setReason(String reason) {
         this.reason = reason;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public String getReason() {
         return reason;
     }
     
+    /**
+     * 
+     * @param verdict 
+     */
     public void setStudentVerdict(String verdict) {
         studentVerdict = verdict;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public String getStudentVerdict() {
         return studentVerdict;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public String toString() {
         return caseTitle + " " + studentVerdict + " " + reason;
     }
