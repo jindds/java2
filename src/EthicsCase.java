9183989b9d8a45d6f495371e1c1594f41f095de2
348337643 <348337643@C-HMCNJH2.tdsb.on.ca>
2026-05-14 11:01:07 -0400

    ethicsCase and verdict classes completed

--- src/EthicsCase.java
+++ src/EthicsCase.java
@@ -8,5 +8,30 @@
  * @author 348337643
  */
 public class EthicsCase {
+    private String caseTitle;
+    private String description;
+    private String category;
+    public Verdict verdict;
     
+    public EthicsCase(String caseTitle, String description, String category) {
+        this.caseTitle = caseTitle;
+        this.description = description;
+        this.category = category;
+    }
+    
+    public String getCaseTitle() {
+        return caseTitle;
+    }
+    
+    public String getCaseDescription() {
+        return description;
+    }
+    
+    public String getCategory() {
+        return category;
+    }
+    
+    public String toString() {
+        return caseTitle + " " + description + " " + category + " " + verdict;
+    }
 }
--- src/Verdict.java
+++ src/Verdict.java
@@ -8,5 +8,47 @@
  * @author 348337643
  */
 public class Verdict {
+    private String caseTitle;
+    private String studentVerdict;
+    private String reason;
     
+    public Verdict() {
+        caseTitle = "";
+        studentVerdict = "";
+        reason = "";
+    }
+    
+    public Verdict(String caseTitle, String verdict, String reason) {
+        this.caseTitle = caseTitle;
+        studentVerdict = verdict;
+        this.reason = reason;
+    }
+    
+    public void setCaseTitle(String title) {
+        caseTitle = title;
+    }
+    
+    public String getCaseTitle() {
+        return caseTitle;
+    }
+    
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+    
+    public String getReason() {
+        return reason;
+    }
+    
+    public void setStudentVerdict(String verdict) {
+        studentVerdict = verdict;
+    }
+    
+    public String getStudentVerdict() {
+        return studentVerdict;
+    }
+    
+    public String toString() {
+        return caseTitle + " " + studentVerdict + " " + reason;
+    }
 }
